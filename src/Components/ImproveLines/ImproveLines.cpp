f1a39780809134455ad991511b8f2f681da1adf9
AniaWujek <aniawujek@gmail.com>
2016-12-04 00:20:06 +0100

    komponent improve lines

---
+++ src/Components/ImproveLines/ImproveLines.cpp
@@ -0,0 +1,259 @@
+/*!
+ * \file
+ * \brief
+ * \author Anna
+ */
+
+#include <memory>
+#include <string>
+
+#include "ImproveLines.hpp"
+#include "Common/Logger.hpp"
+
+#include <boost/bind.hpp>
+
+namespace Processors {
+namespace ImproveLines {
+
+ImproveLines::ImproveLines(const std::string & name) :
+		Base::Component(name) , 
+		collinear_thresh("collinear_thresh", 10),
+		clusters("clusters",1) {
+	registerProperty(collinear_thresh);
+	clusters.addConstraint("1");
+	clusters.addConstraint("10");
+	registerProperty(clusters);
+}
+
+ImproveLines::~ImproveLines() {
+}
+
+void ImproveLines::prepareInterface() {
+	// Register data streams, events and event handlers HERE!
+	registerStream("in_lines", &in_lines);
+	registerStream("in_img", &in_img);
+	registerStream("out_lines", &out_lines);
+	registerStream("out_linesDrawable", &out_linesDrawable);
+	// Register handlers
+	registerHandler("improveLinesProcessor", boost::bind(&ImproveLines::improveLinesProcessor, this));
+	addDependency("improveLinesProcessor", &in_lines);
+	addDependency("improveLinesProcessor", &in_img);
+
+}
+
+bool ImproveLines::onInit() {
+
+	return true;
+}
+
+bool ImproveLines::onFinish() {
+	return true;
+}
+
+bool ImproveLines::onStop() {
+	return true;
+}
+
+bool ImproveLines::onStart() {
+	return true;
+}
+
+// zakladajac, ze linia jest skierowana w gore, czyli kat [0,pi]
+float getAngle(cv::Vec4i line) {
+	float x, y;
+	x = line[2] - line[0];
+	y = -line[3] + line[1];
+	return atan2(y,x);
+}
+
+std::vector<float> getAngles(std::vector<cv::Vec4i> &lines) {
+	std::vector<float> angles;
+	float theta;
+	for(int i = 0; i < lines.size(); ++i) {
+
+		//zeby linie byly skierowane w gore
+		if(lines[i][3]>lines[i][1]) {
+			lines[i] = cv::Vec4i(lines[i][2],lines[i][3],lines[i][0],lines[i][1]);
+		}
+
+		theta = getAngle(lines[i]);		
+
+		angles.push_back(theta);
+	}
+
+	return angles;
+}
+
+float getTriangleArea(float x0, float y0, float x1, float y1, float x2, float y2) {
+	return fabs(((x0-x2)*(y1-y0)-(x0-x1)*(y2-y0))/2.0);
+}
+
+float getLength(cv::Point p0, cv::Point p1) {
+    return sqrt((p0.x-p1.x)*(p0.x-p1.x) + (p0.y-p1.y)*(p0.y-p1.y));
+}
+float getLength(cv::Vec4i line) {
+	return getLength(cv::Point(line[0],line[1]),cv::Point(line[2],line[3]));
+}
+
+bool areCollinear(cv::Vec4i line0, cv::Vec4i line1) {
+	float area = getTriangleArea(line0[0],line0[1],line0[2],line0[3],line1[0],line1[1])+
+					getTriangleArea(line0[0],line0[1],line0[2],line0[3],line1[2],line1[3])+
+					getTriangleArea(line1[0],line1[1],line1[2],line1[3],line0[0],line0[1])+
+					getTriangleArea(line1[0],line1[1],line1[2],line1[3],line0[2],line0[3]);
+	float length = getLength(line0)+getLength(line1);
+	bool collinear = area/length<10.0;
+
+}
+
+cv::Vec4i findLongestLine(vector<cv::Point> &points) {
+	float maxDist = getLength(points[0],points[1]);
+	int max0 = 0;
+	int max1 = 1;
+	for(int i = 0; i < points.size()-1; ++i) {
+		for(int j = i+1; j < points.size(); ++j) {
+			float dist = getLength(points[i],points[j]);
+			if(dist > maxDist) {
+				maxDist = dist;
+				if(points[i].y > points[j].y) {
+					max0 = i;
+					max1 = j;
+				}
+				else {
+					max0 = j;
+					max1 = i;
+				}
+			}
+		} 
+	}
+	cv::Vec4i line = cv::Vec4i(points[max0].x,points[max0].y,points[max1].x,points[max1].y);
+	return line;
+}
+
+
+
+bool areClose(cv::Vec4i line0, cv::Vec4i line1) {
+	float connectorLength = getLength(cv::Point((line0[0]+line0[2])/2.0,(line0[1]+line0[3])/2.0),
+										cv::Point((line1[0]+line1[2])/2.0,(line1[1]+line1[3])/2.0));
+	if(connectorLength < 0.8*(getLength(line0)+getLength(line1))) return true;
+}
+
+void connectLines(std::vector<cv::Vec4i> &lines, std::vector<float> &angles) {
+	//connect lines
+	
+	bool done = false;
+	while(!done) {
+		done = true;
+		for(int l1 = 0; l1 < lines.size()-1; ++l1) {
+			vector<int> to_connect;
+			to_connect.push_back(l1);
+			for(int l2 = l1+1; l2 < lines.size(); ++l2) {
+				bool collinear = true;
+				int c = 0;
+				while(collinear && c < to_connect.size()) {
+					collinear = (fabs(angles[l2] - angles[to_connect[c]])<0.05 || M_PI-fabs(angles[l2] - angles[to_connect[c]])<0.1)
+						&& areCollinear(lines[to_connect[c]],lines[l2])
+						&& areClose(lines[to_connect[c]],lines[l2]);
+					c++;
+				}
+				if(collinear) {
+					to_connect.push_back(l2);
+				}
+			}//end for l2
+			if(to_connect.size()>1) {
+				done = false;
+				std::vector<cv::Point> points;
+				for(int c = 0; c < to_connect.size(); ++c) {
+					points.push_back(cv::Point(lines[to_connect[c]][0],lines[to_connect[c]][1]));
+					points.push_back(cv::Point(lines[to_connect[c]][2],lines[to_connect[c]][3]));
+				}
+				lines[l1] = findLongestLine(points);
+				angles[l1] = getAngle(lines[l1]);
+
+				for(int c = to_connect.size()-1; c > 0; --c) {
+					lines.erase(lines.begin()+to_connect[c]);
+					angles.erase(angles.begin()+to_connect[c]);
+				}
+
+			}
+		}// end for l1
+
+	}// end while
+
+}
+
+void removeShort(std::vector<cv::Vec4i> &lines, int rows, int cols) {
+	float thresh = float(min(cols, rows))*0.2;
+	for(int i = lines.size()-1; i >= 0; --i) {
+		if(getLength(lines[i]) < thresh) {
+			lines.erase(lines.begin()+i);
+		}
+	}
+
+}
+
+float getMean(std::vector<float> &data) {
+	float sum = 0.0;
+	for(int i = 0; i<data.size(); ++i) {
+		sum += data[i];
+	}
+	return sum/float(data.size());
+}
+
+float getVariance(std::vector<float> &data) {
+
+	float mean = getMean(data);
+	float sumSquareSub = 0.0;
+	for(int i = 0; i<data.size(); ++i) {
+		sumSquareSub += (data[i]-mean)*(data[i]-mean);
+	}
+	return sumSquareSub/float(data.size());
+
+}
+
+void leaveVerticalPerspective(std::vector<cv::Vec4i> &lines, std::vector<float> &angles, int clusters) {
+	cv::Mat mat_angles(angles.size(), 1, CV_32FC1);
+	for(int i = 0; i < angles.size(); ++i) {
+		mat_angles.at<float>(i) = angles[i];
+	}
+	cv::Mat centers, labels;
+	cv::kmeans(mat_angles,clusters,labels,
+		cv::TermCriteria(cv::TermCriteria::EPS+cv::TermCriteria::COUNT,
+			10, M_PI/float(clusters)),3,cv::KMEANS_PP_CENTERS, centers);
+
+	std::vector<std::vector<float> > angle_groups;
+
+
+
+}
+
+void ImproveLines::improveLinesProcessor() {
+
+	std::vector<cv::Vec4i> lines = in_lines.read();
+	cv::Mat img = in_img.read().clone();
+	std::vector<float> angles = getAngles(lines);
+
+	connectLines(lines,angles);
+	removeShort(lines, img.rows, img.cols);
+
+
+	
+
+	Types::DrawableContainer c;
+	for( size_t i = 0; i < lines.size(); i++ )
+	{
+        c.add(new Types::Line(cv::Point(lines[i][0], lines[i][1]),
+            cv::Point(lines[i][2], lines[i][3]), cv::Scalar(255,0,0)));		
+	}
+
+	out_lines.write(lines);
+	out_linesDrawable.write(c);
+
+
+	
+
+}
+
+
+
+} //: namespace ImproveLines
+} //: namespace Processors
---
+++ src/Components/ImproveLines/ImproveLines.hpp
@@ -0,0 +1,100 @@
+/*!
+ * \file
+ * \brief 
+ * \author Anna
+ */
+
+#ifndef IMPROVELINES_HPP_
+#define IMPROVELINES_HPP_
+
+#include "Component_Aux.hpp"
+#include "Component.hpp"
+#include "DataStream.hpp"
+#include "Property.hpp"
+#include "EventHandler2.hpp"
+#include "Types/Line.hpp"
+#include "Types/DrawableContainer.hpp"
+
+#include <opencv2/opencv.hpp>
+
+
+namespace Processors {
+namespace ImproveLines {
+
+/*!
+ * \class ImproveLines
+ * \brief ImproveLines processor class.
+ *
+ * 
+ */
+class ImproveLines: public Base::Component {
+public:
+	/*!
+	 * Constructor.
+	 */
+	ImproveLines(const std::string & name = "ImproveLines");
+
+	/*!
+	 * Destructor
+	 */
+	virtual ~ImproveLines();
+
+	/*!
+	 * Prepare components interface (register streams and handlers).
+	 * At this point, all properties are already initialized and loaded to 
+	 * values set in config file.
+	 */
+	void prepareInterface();
+
+protected:
+
+	/*!
+	 * Connects source to given device.
+	 */
+	bool onInit();
+
+	/*!
+	 * Disconnect source from device, closes streams, etc.
+	 */
+	bool onFinish();
+
+	/*!
+	 * Start component
+	 */
+	bool onStart();
+
+	/*!
+	 * Stop component
+	 */
+	bool onStop();
+
+
+	// Input data streams
+	Base::DataStreamIn<std::vector<cv::Vec4i> > in_lines;
+	Base::DataStreamIn<cv::Mat> in_img;
+
+	// Output data streams
+	Base::DataStreamOut<std::vector<cv::Vec4i> > out_lines;
+	Base::DataStreamOut <Types::DrawableContainer> out_linesDrawable;
+
+	// Handlers
+
+	// Properties
+	Base::Property<float> collinear_thresh;
+	Base::Property<int> clusters;
+
+	
+	// Handlers
+	void improveLinesProcessor();
+
+};
+
+} //: namespace ImproveLines
+} //: namespace Processors
+
+/*
+ * Register processor component.
+ */
+REGISTER_COMPONENT("ImproveLines", Processors::ImproveLines::ImproveLines)
+
+#endif /* IMPROVELINES_HPP_ */
